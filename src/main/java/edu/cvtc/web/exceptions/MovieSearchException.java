cb2588b97b5cdfc5023a6fbd7eff6a694cd2012b
Greg Cedarblade <gmcedarblade@gmail.com>
2016-12-13 15:14:19 -0600

    added movie search exception class

---
+++ src/main/java/edu/cvtc/web/exceptions/MovieSearchException.java
@@ -0,0 +1,15 @@
+package edu.cvtc.web.exceptions;
+
+/**
+ * @author gcedarblade
+ *
+ */
+public class MovieSearchException extends Exception {
+
+	private static final long serialVersionUID = 7229099228111199403L;
+	
+	public MovieSearchException(final String message) {
+		super(message);
+	}
+
+}
